4d10623e2fb474f97655e253d91e8c91568c39dc
Abbosbek <abbosbekqodirov2501@gmail.com>
2024-05-29 09:23:36 +0500

    start

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="container">
+        <h1>Navbar</h1>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
