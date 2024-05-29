d3a428e4547ab7a5e18aeb55437ec6e575d25872
Abbosbek <abbosbekqodirov2501@gmail.com>
2024-05-29 10:39:38 +0500

    home tugadi

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import "./Navbar.css"
 
 function Navbar() {
   return (
@@ -6,6 +8,34 @@ function Navbar() {
       <div className="container">
         <div className="logo">
           <img src="./imgs/logo.png" alt="" />
+        </div>
+        <ul className="links">
+          <li>
+            <Link to={"/"}>SHOP</Link>
+          </li>
+          <li>
+            <Link to={"/"}>COLLECTIVE</Link>
+          </li>
+          <li>
+            <Link to={"/"}>DESIGNERS</Link>
+          </li>
+          <li>
+            <Link to={"/"}>ABOUT US</Link>
+          </li>
+          <li>
+            <Link to={"/"}>CONTACT</Link>
+          </li>
+        </ul>
+        <div className="menu">
+          <div>
+            <img src="./imgs/menu.svg" alt="" />
+          </div>
+          <div>
+            <img src="./imgs/search.svg" alt="" />
+          </div>
+          <div className='cart_icon'>
+            <img src="./imgs/shopping-cart.svg" alt="" />
+          </div>
         </div>
       </div>
     </nav>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,90 @@
 import React from 'react'
+import { FaArrowLeftLong, FaRegHeart } from "react-icons/fa6";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaPinterest,
+  FaInstagram,
+} from "react-icons/fa";
+import {Link} from "react-router-dom"
+import "./Home.css"
 
 function Home() {
   return (
-    <div>
+    <div className="hero">
       <div className="container">
-        <h1>Home</h1>
+        <div className="hero_info">
+          <div className="navigate">
+            <FaArrowLeftLong />
+          </div>
+          <p className='location'>
+            <span>Chair</span> / <b>Meryl Lounge Chair</b>
+          </p>
+          <h1 className='product_title'>Meryl Lounge Chair</h1>
+          <div className='price'>
+            <h2>$149.99</h2>
+            <img src="./imgs/raitings.png" alt="" />
+          </div>
+          <p className="desc">
+            The gently curved lines accentuated by sewn details are kind to your
+            body and pleasant to look at. Also, there’s a tilt and
+            height-adjusting mechanism that’s built to outlast years of ups and
+            downs.
+          </p>
+          <div className="colors">
+            {["#C1BDB3", "#58737D", "#545454", "#CBA5A5"].map((item) => {
+              return <span style={{ backgroundColor: item }}></span>;
+            })}
+          </div>
+          <div className="add_Cart">
+            <div className="count">
+              <span className='minus'>-</span>
+              <span>1</span>
+              <span className='plus'>+</span>
+            </div>
+            <button>Add to Cart</button>
+          </div>
+          <p>Free 3-5 day shipping • Tool-free assembly • 30-day trial</p>
+          <div className="social">
+            <div className="like">
+              <FaRegHeart />
+              Add to Wishlist
+            </div>
+            <div className="social_links">
+              <Link
+                target="_blank"
+                to={"https://www.facebook.com/abbosbek.qodirov.319/"}
+              >
+                <FaFacebook />
+              </Link>
+              <Link to={"/"}>
+                <FaTwitter />
+              </Link>
+              <Link
+                target="_blank"
+                to={"https://www.pinterest.com/abbosbekqodirov2501/"}
+              >
+                <FaPinterest />
+              </Link>
+              <Link
+                target="_blank"
+                to={"https://www.instagram.com/abbosbek_qodirovv/"}
+              >
+                <FaInstagram />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="hero_imgs">
+          <div>
+            <img src="./imgs/main_img.png" alt="" />
+          </div>
+          <div className="imgs">
+            {[1, 2, 3, 4, 5].map((item) => {
+              return <img src={`./imgs/img${item}.png`} alt="" />;
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
