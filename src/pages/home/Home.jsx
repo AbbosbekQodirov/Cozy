de3e40b334df9ba4a9fc6fb57d5a2d83b2b60456
Abbosbek <abbosbekqodirov2501@gmail.com>
2024-05-29 09:38:12 +0500

    home yangilandi

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="container">
+        <h1>Home</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Home
